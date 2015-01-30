947d08d71f22be4bff3c1d9e785ed08344438d95
dujuanxian <jdu.xian@gmail.com>
2015-01-30 10:27:39 +0800

    remove footer and useless content

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create('dist/index.html', {
+        'id': 'webpanda',
         'bounds': {
             'width': 960,
             'height': 640
