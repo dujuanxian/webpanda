052e1959bbd4f2afba87ff07dbbd7317a966caa4
Peng.Li <peng.li@rea-group.com>
2015-02-01 23:32:51 +0800

    Use `bind(this)` to avoid `that`. Peng.Li

--- app/scripts/ui/Sidebar.js
+++ app/scripts/ui/Sidebar.js
@@ -7,10 +7,9 @@ var Sidebar = React.createClass({
         this.props.onFileClick(fileName);
     },
     render: function() {
-        var that = this;
         var files = this.props.fileNames.map(function(fileName) {
-            return <File onClick={that.onClick} fileName={fileName}/>;
-        });
+            return <File onClick={this.onClick} fileName={fileName}/>;
+        }.bind(this));
         return (
             <section className="sidebar">
                 <h5>Project</h5>
